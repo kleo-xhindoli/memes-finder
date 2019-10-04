268dfdfd51fef292f74f74f845d205269d82a799
kxhindoli <kxhindoli@ritech.co>
2019-10-04 20:09:31 +0200

    add mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import { config } from 'dotenv';
+import Koa from 'koa';
+import logger from 'koa-logger';
+import mongoose from 'mongoose';
+import helmet from 'koa-helmet';
 
 config();
 
 const port = process.env.PORT;
+const connectionString = process.env.MONGODB_CONNECTION as string;
+
+mongoose.connect(connectionString);
+mongoose.connection.on('error', console.error);
 
 const app = new Koa();
+
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(helmet());
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
